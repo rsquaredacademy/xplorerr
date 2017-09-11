486cde3785cb0f06d1f27f407d192e2bc24ff77f
rsquaredin <rsquaredin@gmail.com>
2017-09-11 11:38:16 +0530

    add launch app function

---
+++ R/launch.R
@@ -0,0 +1,12 @@
+#' @importFrom shiny runApp
+#' @title Launch App
+#' @description Launches the xplorerr app
+#' @examples
+#' \dontrun{
+#' launch_xplorerr()
+#' }
+#' @export
+#'
+launch_xplorerr <- function() {
+  shiny::runApp(appDir = system.file("application", package = 'xplorerr'))
+}
---
+++ R/xplorerr.R
@@ -0,0 +1,13 @@
+#' \code{olsrr} package
+#'
+#' R Shiny app for interactive statistical analysis
+#'
+#' See the README on
+#' \href{https://github.com/rsquaredacademy/xplorerr}{GitHub}
+#'
+#' @docType package
+#' @name xplorerr
+NULL
+
+## quiets concerns of R CMD check re: the .'s that appear in pipelines
+if(getRversion() >= "2.15.1")  utils::globalVariables(c("."))
