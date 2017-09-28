add81344033b68409cdc1da53d2e0526d2787529
rsquaredin <rsquaredin@gmail.com>
2017-09-28 22:17:34 +0530

    remove variable selection tab from xplorerr app

--- inst/app-xplorerr/server.R
+++ inst/app-xplorerr/server.R
@@ -70,7 +70,7 @@ shinyServer(function(input, output, session) {
     source("logic/logic_pie3d.R", local = T)
     source("logic/logic_scatter.R", local = T)
     source("logic/logic_regress.R", local = T)
-    source("logic/logic_mselection2.R", local = T)
+    # source("logic/logic_mselection2.R", local = T)
     source("logic/logic_resdiagtrial.R", local = T)
     source("logic/logic_hetero.R", local = T)
     source("logic/logic_collin.R", local = T)
