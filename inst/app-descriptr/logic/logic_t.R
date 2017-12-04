3f742c7f26c9b4ffdf18df0fb527945d75719c08
rsquaredin <rsquaredin@gmail.com>
2017-12-04 23:24:38 +0530

    remove deprecated functions from descriptr app

--- inst/app-descriptr/logic/logic_t.R
+++ inst/app-descriptr/logic/logic_t.R
@@ -1,11 +1,11 @@
 output$t_shape <- renderPlot({
-  t_plot(input$t_df)
+  dist_t_plot(input$t_df)
 })
 
 output$tprob_plot <- renderPlot({
-  t_prob(input$tprob_p, input$tprob_df, input$tprob_tail)
+  dist_t_prob(input$tprob_p, input$tprob_df, input$tprob_tail)
 })
 
 output$tperc_plot <- renderPlot({
-  t_per(input$tperc_p, input$tperc_df, input$tperc_tail)
+  dist_t_perc(input$tperc_p, input$tperc_df, input$tperc_tail)
 })
