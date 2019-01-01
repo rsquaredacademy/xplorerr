048d1364f9df35cbe40abce40f0805941ea55e71
rsquaredin <rsquaredin@gmail.com>
2019-01-01 19:57:32 +0530

    modify name

--- inst/app-visualize/ui.R
+++ inst/app-visualize/ui.R
@@ -5,7 +5,7 @@ library(descriptr)
 library(dplyr)
 
 shinyUI(	
-  navbarPage(HTML("Xplorerr - Interactive Data Visualization"), id = 'mainpage',
+  navbarPage(HTML("visualizer"), id = 'mainpage',
     source('ui/ui_data.R', local = TRUE)[[1]],
     source('ui/ui_visualize.R', local = TRUE)[[1]],
     source('ui/ui_exit_button.R', local = TRUE)[[1]]
