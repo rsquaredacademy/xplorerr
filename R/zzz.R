a6be2309dfa6915ad8cb34ab334a8c58d6a0f3f6
rsquaredin <rsquaredin@gmail.com>
2019-01-04 12:55:42 +0530

    prep for cran release

--- R/zzz.R
+++ R/zzz.R
@@ -2,9 +2,10 @@
   if (!interactive() || stats::runif(1) > 0.1) return()
 
   tips <- c(
-    "Learn more about xplorerr at http://github.com/rsquaredacademy/xplorerr/.",
+    "Learn more about xplorerr at https://github.com/rsquaredacademy/xplorerr/.",
     "Use suppressPackageStartupMessages() to eliminate package startup messages.",
-    "Need help getting started with data science? Visit: https://www.rsquaredacademy.com"
+    "Need help getting started with regression models? Visit: https://www.rsquaredacademy.com",
+    "Check out our interactive app for quick data exploration. Visit: https://apps.rsquaredacademy.com/."
   )
 
   tip <- sample(tips, 1)
